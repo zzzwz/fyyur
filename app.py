5447175787007b0011efc8598331800746327e44
zheng <zhengwangsea@gmail.com>
2019-09-22 17:43:45 -0700

    .gitnorore fix 2

--- app.py
+++ app.py
@@ -43,8 +43,16 @@ class Venue(db.Model):
     facebook_link = db.Column(db.String(120))
     rating = db.Column(db.String(120))
 
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    # TODO: implement any missing fields, as a database migration using Flask-Migrate - checked
 
+    # added fields
+    website = db.Column(db.String(200))
+    genres = db.Column(db.ARRAY(db.String(120)))
+    seeking_talent = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String(1000))
+
+
+  
 class Artist(db.Model):
     __tablename__ = 'Artist'
 
@@ -53,13 +61,21 @@ class Artist(db.Model):
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
+    # change type genres to array of strings
+    genres = db.Column(db.ARRAY(db.String(120)))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
 
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    # TODO: implement any missing fields, as a database migration using Flask-Migrate - checked
+
+    # added fields
+    seeking_venue = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String(1000))
+    
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+
+
 
 #----------------------------------------------------------------------------#
 # Filters.
--- config.py
+++ config.py
@@ -5,6 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Enable debug mode.
 DEBUG = True
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # Connect to the database
 
